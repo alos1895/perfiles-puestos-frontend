a1de25250e0cdda874761ffbf74ee3232d14c042
Alonso Sanchez <alonso.sanchez@jalisco.gob.mx>
2022-02-25 10:27:05 -0600

    Vistas creadas

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: []
+})
+export class RegisterComponent implements OnInit {
+  email?: string;
+  password?: string;
+  confirmPassword? : string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  register(){
+    console.log(this.email);
+    console.log(this.password);
+    console.log(this.confirmPassword);
+  }
+
+}
